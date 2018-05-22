1eeb2ae7e0c1de2f9c2b753a9a4bbe7e79503a2f
daxupaxu <dawid.daoxuan@gmail.com>
2018-05-22 18:07:36 +0200

    adding css and js

--- functions.php
+++ functions.php
@@ -1,3 +1,3 @@
 
 <?php wp_enqueue_style('customstyle', get_template_directory_uri() . 'assets/css/style.css', array=(), 'false', 'all' ); ?>
-<?php wp_enqueue_style('customjs', get_template_directory_uri() . 'assets/js/js.js', array=(), 'false', 'all' ); ?>git  
+<?php wp_enqueue_style('customjs', get_template_directory_uri() . 'assets/js/js.js', array=(), 'false', 'all' ); ?>git      
